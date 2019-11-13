9f91ada6b8e224f65776a08180519009e66a84c4
Mike <troianello@protonmail.com>
2019-11-12 23:07:12 -0800

    login page arranged into proper card. Logs also are arranged via flex-evenly.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,9 +3,10 @@ import AuthService from '../components/auth/auth-service';
 
 export default class Login extends Component {
   state = {
+    message: null,
     user: '',
-    username: 'mike',
-    password: 'mike'
+    username: '',
+    password: ''
   };
   service = new AuthService();
 
@@ -20,43 +21,71 @@ export default class Login extends Component {
     e.preventDefault();
     const username = this.state.username;
     const password = this.state.password;
-    this.service
-      .login(username, password)
-      .then(results => {
-        this.setState({ username: '', password: '' });
+    if (!username) {
+      console.log('NO username');
+      this.setState({
+        message: `You must include a username`
+      });
+    } else if (!password) {
+      console.log('NO PASSWORD');
+      this.setState({
+        message: `You must include a password`
+      });
+    } else {
+      this.service
+        .login(username, password)
+        .then(results => {
+          this.setState({ username: '', password: '' });
 
-        this.props.getUser(results);
-        localStorage.setItem('user', JSON.stringify(results));
+          this.props.getUser(results);
+          localStorage.setItem('user', JSON.stringify(results));
 
-        this.props.history.push('/');
-      })
-      .catch(error => console.log(error));
+          this.props.history.push('/');
+        })
+        .catch(error => {
+          console.log(error);
+          this.setState({
+            message: `Incorrect Username or Password`
+          });
+        });
+    }
   };
 
   render() {
     return (
       <div>
-        This is the Login Page
-        <form onSubmit={this.handleSubmit}>
-          <div className='form-piece'>
-            <label htmlFor='username'>Username:</label>
-            <input
-              name='username'
-              placeholder='Your name...'
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className='form-piece'>
-            <label htmlFor='password'>Password:</label>
-            <input
-              name='password'
-              placeholder='******'
-              type='password'
-              onChange={this.handleChange}
-            />
-          </div>
-          <button>Submit</button>
-        </form>
+        <div className='signup'>
+          <span className='signup-header'>This is the Login Page</span>
+          <form onSubmit={this.handleSubmit}>
+            <div className='form-piece login-form-piece'>
+              <label htmlFor='username'>Username:</label>
+              <input
+                type='text'
+                style={{ 'font-size': '1em', width: '55%' }}
+                name='username'
+                placeholder='Your name...'
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className='form-piece login-form-piece'>
+              <label htmlFor='password'>Password:</label>
+              <input
+                name='password'
+                style={{ 'font-size': '1em', width: '55%' }}
+                placeholder='******'
+                type='password'
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className='login-button'>
+              <button className=''>Submit</button>
+            </div>
+          </form>
+          <b className='signup-message'>{this.state.message}</b>
+        </div>
+        <div className='switch-button'>
+          <button className=''>Click to Create an Account</button>
+        </div>
       </div>
     );
   }
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -53,62 +53,67 @@ export default class Signup extends Component {
 
   render() {
     return (
-      <div className='signup'>
-        <span className='signup-header'>This is the Signup Page</span>
-        <form className='signup-form' onSubmit={this.handleSubmit}>
-          <div className='form-pieces'>
-            <div className='form-piece'>
-              <label htmlFor='name'>Your Username:</label>
-              <input
-                name='username'
-                placeholder='Your name...'
-                onChange={this.handleChange}
-              />
+      <div>
+        <div className='signup'>
+          <span className='signup-header'>This is the Signup Page</span>
+          <form className='signup-form' onSubmit={this.handleSubmit}>
+            <div className='form-pieces'>
+              <div className='form-piece'>
+                <label htmlFor='name'>Your Username:</label>
+                <input
+                  name='username'
+                  placeholder='Your name...'
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className='form-piece'>
+                <label htmlFor='password'>Password:</label>
+                <input
+                  name='password'
+                  type='password'
+                  placeholder='Your password...'
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className='form-piece'>
+                <label htmlFor='email'>Email: (optional)</label>
+                <input
+                  name='email'
+                  type='email'
+                  placeholder='Your email...'
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className='form-piece'>
+                <label htmlFor='phone'>Phone: (optional)</label>
+                <input
+                  name='phone'
+                  type='number'
+                  placeholder='867-5309'
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className='form-piece'>
+                <label htmlFor='gender'>What is your gender?</label>
+                <select name='gender' onChange={this.handleChange}>
+                  <option selected disabled>
+                    Choose:
+                  </option>
+                  <option value='male'>Male</option>
+                  <option value='female'>Female</option>
+                  <option value='non-binary'>Non-binary</option>
+                </select>
+              </div>
             </div>
-            <div className='form-piece'>
-              <label htmlFor='password'>Password:</label>
-              <input
-                name='password'
-                type='password'
-                placeholder='Your password...'
-                onChange={this.handleChange}
-              />
+            <div className='signup-button'>
+              <button className=''>Submit</button>
             </div>
-            <div className='form-piece'>
-              <label htmlFor='email'>Email: (optional)</label>
-              <input
-                name='email'
-                type='email'
-                placeholder='Your email...'
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className='form-piece'>
-              <label htmlFor='phone'>Phone: (optional)</label>
-              <input
-                name='phone'
-                type='number'
-                placeholder='867-5309'
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className='form-piece'>
-              <label htmlFor='gender'>What is your gender?</label>
-              <select name='gender' onChange={this.handleChange}>
-                <option selected disabled>
-                  Choose:
-                </option>
-                <option value='male'>Male</option>
-                <option value='female'>Female</option>
-                <option value='non-binary'>Non-binary</option>
-              </select>
-            </div>
-          </div>
-          <div className='signup-button'>
-            <button className=''>Submit</button>
-          </div>
-        </form>
-        <b className='signup-message'>{this.state.message}</b>
+          </form>
+          <b className='signup-message'>{this.state.message}</b>
+        </div>
+        <div className='switch-button'>
+          <button className=''>Click to Create an Account</button>
+        </div>
       </div>
     );
   }
